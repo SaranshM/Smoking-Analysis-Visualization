b56849818b6c6753ad8544172e8a823f7fc171b8
Saransh Mehta <saranshmehta96@gmail.com>
2020-10-27 19:07:02 +0530

    init

--- js/malevfemale.js
+++ js/malevfemale.js
@@ -1,3 +1,6 @@
+var myChart
+var myChart2
+
 const year = ['1984', '1985', '1986', '1987', '1988', '1989', '1990', '1991', '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018']
 const male = [26.9, 28.2, 28.0, 22.8, 25.6, 22.5, 21.1, 22.7, 22.3, 20.7, 18.9, 18.7, 20.8, 21.1, 20.6, 19.7, 18.7, 20.2, 18.8, 18.4, 18.2, 17.0, 17.5, 17.1, 16.5, 15.8, 14.7, 14.9, 15.5, 15.1, 14.9, 12.9, 14.8, 13.3, 12.1]
 const female = [23.0, 25.2, 23.3, 20.1, 19.9, 19.8, 17.9, 15.8, 17.8, 15.8, 14.4, 13.2, 14.8, 13.9, 14.4, 14.6, 13.9, 12.7, 12.8, 12.5, 11.1, 11.1, 9.1, 10.7, 10.6, 10.5, 9.6, 9.3, 10.0, 8.5, 8.4, 8.1, 8.1, 7.0, 7.5]
@@ -38,7 +41,7 @@ male_smoking.addEventListener("click",() => {
     document.getElementsByTagName("canvas")[0].style.display = "block";
     document.getElementsByTagName("canvas")[1].style.display = "block";
     var ctx = document.getElementById('myChart').getContext('2d');
-    var myChart = new Chart(ctx, {
+    myChart = new Chart(ctx, {
         type: 'radar',
         data: {
             labels: year,
@@ -93,7 +96,7 @@ male_smoking.addEventListener("click",() => {
     });
 
     var ctx2 = document.getElementById('myChart2').getContext('2d');
-    var myChart2 = new Chart(ctx2, {
+    myChart2 = new Chart(ctx2, {
         type: 'pie',
         data: {
             labels: decades,
@@ -153,7 +156,7 @@ female_smoking.addEventListener("click",() => {
     document.getElementsByTagName("canvas")[0].style.display = "block";
     document.getElementsByTagName("canvas")[1].style.display = "block";
     var ctx = document.getElementById('myChart').getContext('2d');
-    var myChart = new Chart(ctx, {
+    myChart = new Chart(ctx, {
         type: 'radar',
         data: {
             labels: year,
@@ -208,7 +211,7 @@ female_smoking.addEventListener("click",() => {
     });
 
     var ctx2 = document.getElementById('myChart2').getContext('2d');
-    var myChart2 = new Chart(ctx2, {
+    myChart2 = new Chart(ctx2, {
         type: 'doughnut',
         data: {
             labels: decades,
@@ -268,7 +271,7 @@ malevfemale.addEventListener("click",() => {
     document.getElementsByTagName("canvas")[0].style.display = "block";
     document.getElementsByTagName("canvas")[1].style.display = "block";
     var ctx = document.getElementById('myChart').getContext('2d');
-    var myChart = new Chart(ctx, {
+    myChart = new Chart(ctx, {
         type: 'line',
         data: {
             labels: year,
@@ -307,7 +310,7 @@ malevfemale.addEventListener("click",() => {
     });
 
     var ctx2 = document.getElementById('myChart2').getContext('2d');
-    var myChart = new Chart(ctx2, {
+    myChart = new Chart(ctx2, {
         type: 'radar',
         data: {
             labels: year,
